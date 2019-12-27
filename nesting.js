4dc89e29d3b0e062fb24b4c61bbee9a3b87f4afb
Nick Rosa <nicholascastrorosa@gmail.com>
2019-12-26 18:13:36 -0700

    Finished nesting

--- nesting.js
+++ nesting.js
@@ -50,7 +50,21 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+function employeeUpdater(){
+  for (let i = 0; i < employees.length; i++){
+    if (employees[i].firstName === 'Theo'){
+     employees.splice(i, 1)
+     i--
+    }
+    
+    else if (employees[i].firstName === 'Lorie'){
+      employees[i].department = 'HR'
+    }
+  }
+  return employees
+}
+  
+
 
 
 
@@ -68,7 +82,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+function removeDuplicates(){
+  for (let i = 0; i < workplaceAccidents.length; i++){
+    for (let j = i + 1; j < workplaceAccidents.length; j++){
+      if (workplaceAccidents[i] === workplaceAccidents[j]){
+        workplaceAccidents.splice(j, 1)
+      } 
+    }
+  }
+  return workplaceAccidents
+}
 
 
 
@@ -96,9 +119,9 @@ var cat = {
     2. Assign fluffy2ndFriend the name of Fluffy's 2nd friend.
 */
 
-//Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -138,7 +161,22 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
+function recordCleaner(){
+  for (let i = 0; i < myCar.accidents.length; i++){
+    if (myCar.accidents[i].atFaultForAccident === true){
+      myCar.accidents[i].atFaultForAccident = false
+    }
+  }
+}
+
+// OR
+
+// function recordCleaner()[
+//   let accd = myCar.accidents
+//   for (let i = 0; i < accd.length; i++){
+//     accd[i].atFaultForAccident = false
+//   }
+// ]
 
 
 
@@ -157,6 +195,17 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
+function looper(){
+  for (let i = 0; i < numsArr.length; i++){
+    for (let j = 0; j < numsArr[i].length; j++){
+      if (numsArr[i][j] % 2 === 0){
+        numsArr[i][j] = 'even'
+      } else {
+        numsArr[i][j] = 'odd'
+      }
+    }
+  }
+  return numsArr
+}
 
 
